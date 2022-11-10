4ca24d2e30001742fda826190db97921be06c22b
Rex Jiang <61212763+RexJiangEvoBio@users.noreply.github.com>
2022-11-09 16:46:54 -0800

    Update sim_interaction_3g.R
    Optimized to improve efficiency.

--- test/sim_interaction_3g.R
+++ test/sim_interaction_3g.R
@@ -11,34 +11,13 @@ ngene=3 # Number of genes under consideration
 # Numbers represent elements [1,2], [1,3], [2,1], [2,3], [3,1], [3,2], respectively
 permut.all=permutations(3,6,c(-0.5,0,0.5),repeats.allowed=TRUE)
 
-# Get transpose of 3x3 matrix given off-diagonal elements
-# "tpv" for "'transpose' of vector"
-tpv <- function(v){
-	new=rep(0,6)
-	new[1]=v[3];new[3]=v[1]
-	new[2]=v[5];new[5]=v[2]
-	new[4]=v[6];new[6]=v[4]
-	return(new)
-}
-# Remove redundant combinations
-coeff.all=list()
-num=0
-for(i in 1:nrow(permut.all)){
-	check=list(tpv(permut.all[i,])) %in% coeff.all
-	if(check==FALSE){
-		num=num+1
-		coeff.all[[num]]=permut.all[i,]
-	}
-}
-
-# Convert to matrix format
-coeff.all.m=list()
-for(i in 1:length(coeff.all)){
+# Covert vector of interaction paramters to matrix format
+v2m <- function(v){
 	m=matrix(0,nrow=3,ncol=3)
-	m[1,2:3]=coeff.all[[i]][1:2]
-	m[2,1]=coeff.all[[i]][3];m[2,3]=coeff.all[[i]][4]
-	m[3,1:2]=coeff.all[[i]][5:6]
-	coeff.all.m[[i]]=m
+	m[1,2:3]=v[1:2]
+	m[2,1]=v[3];m[2,3]=v[4]
+	m[3,1:2]=v[5:6]
+	return(m)
 }
 
 # Fitness function
@@ -48,10 +27,11 @@ fitness <- function(d,a){
 	for(i in 1:length(d)){
 		if(a[i]!=0){
 			w[i]=dnorm(d[i],mean=0,sd=a[i])/dnorm(0,mean=0,sd=a[i]) # Calculate a fitness value for each trait
+		}else{
+			w[i]=1
 		}
 	}
-	w=w[which(a!=0)] # Remove traits that do not affect fitness
-	w=prod(w)^(1/length(w)) # Overall fitness calculated as geometric mean of fitness values calculated from individual traits
+	w=prod(w)^(1/length(which(a!=0))) # Overall fitness calculated as geometric mean of fitness values calculated from individual traits
 	return(w)
 }
 
@@ -115,82 +95,85 @@ for(i in 1:ngene){
 
 u=rep(1,2*ngene) # Mutation rates for each trait
 sigma=rep(0.1,2*ngene) # SD of mutation effect size
-width=rep(1,ngene) # Width of fitness function for each ngene
+width=c(0,0,1) # Width of fitness function for each ngene
 opt=rep(0,ngene)
 
 T=1e4 # Duration of simulation for each lineage
 Nrep=100 # Number of lineages
 
-for(ncc in 1:length(coeff.all.m)){ # Go through the list of interaction matrices
+out.all=matrix(0,nrow=nrow(permut.all)*T/10,ncol=28)
 
-coeff=coeff.all.m[[ncc]]
-gt=list();pt=list()
-
-for(n in 1:Nrep){
-	gt[[n]]=matrix(0,nrow=2*ngene,ncol=(T+1)) # Mean genotypic values of the n-th lineage through time 
-	pt[[n]]=matrix(0,nrow=2*ngene,ncol=(T+1)) # Mean phenotypes of the n-th lineage through time 
-	pt[[n]][,1]=g2p(gt[[n]][,1],coeff) # Calculate the starting phenotype
-	for(t in 2:(T+1)){
-		gt[[n]][,t]=gt[[n]][,(t-1)]
-		nm=rpois(1,lambda=sum(u)) # Total number of mutations that would occur in this time step
-		if(nm>0){
-			for(i in 1:nm){
-				gt_ances=gt[[n]][,t]
-				pt_ances=g2p(gt_ances,coeff) # Calcuate ancestral phenotype from the ancestral genotype
-				gt_mutant=gt_ances
-				type=sample(1:(2*ngene),1,prob=u/sum(u)) # Decide which trait the mutation affects
-				effect=rnorm(1,mean=0,sd=sigma[type])
-				gt_mutant[type]=gt_mutant[type]+effect # Add the mutation's effect to the affected trait
-				pt_mutant=g2p(gt_mutant,coeff) # Calculate mutant phenotype from the mutant genotype
-				pf=fix.prob(pt_ances[row.protein],pt_mutant[row.protein],width) # Fixation probability of the mutation
-				if.fix=rbinom(n=1,size=1,prob=pf) # Decide if the mutation would fix
-				if(if.fix==1){
-					gt[[n]][,t]=gt_mutant # If the mutation is fixed, the mutant genotype would become the ancestral phenotype when the next mutation is examined
+row=1
+for(ncc in 1:nrow(permut.all)){
+	coeff=v2m(permut.all[ncc,])
+	out.all[row:(row+T/10-1),1]=permut.all[ncc,1]
+	out.all[row:(row+T/10-1),2]=permut.all[ncc,2]
+	out.all[row:(row+T/10-1),3]=permut.all[ncc,3]
+	out.all[row:(row+T/10-1),4]=permut.all[ncc,4]
+	out.all[row:(row+T/10-1),5]=permut.all[ncc,5]
+	out.all[row:(row+T/10-1),6]=permut.all[ncc,6]
+	gt=list();pt=list()
+	for(n in 1:Nrep){
+		gt[[n]]=matrix(0,nrow=2*ngene,ncol=(T+1)) # Mean genotypic values of the n-th lineage through time 
+		pt[[n]]=matrix(0,nrow=2*ngene,ncol=(T+1)) # Mean phenotypes of the n-th lineage through time 
+		pt[[n]][,1]=g2p(gt[[n]][,1],coeff) # Calculate the starting phenotype
+		for(t in 2:(T+1)){
+			gt[[n]][,t]=gt[[n]][,(t-1)]
+			nm=rpois(1,lambda=sum(u)) # Total number of mutations that would occur in this time step
+			if(nm>0){
+				for(i in 1:nm){
+					gt_ances=gt[[n]][,t]
+					pt_ances=g2p(gt_ances,coeff) # Calcuate ancestral phenotype from the ancestral genotype
+					gt_mutant=gt_ances
+					type=sample(1:(2*ngene),1,prob=u/sum(u)) # Decide which trait the mutation affects
+					effect=rnorm(1,mean=0,sd=sigma[type])
+					gt_mutant[type]=gt_mutant[type]+effect # Add the mutation's effect to the affected trait
+					pt_mutant=g2p(gt_mutant,coeff) # Calculate mutant phenotype from the mutant genotype
+					pf=fix.prob(pt_ances[row.protein],pt_mutant[row.protein],width) # Fixation probability of the mutation
+					if.fix=rbinom(n=1,size=1,prob=pf) # Decide if the mutation would fix
+					if(if.fix==1){
+						gt[[n]][,t]=gt_mutant # If the mutation is fixed, the mutant genotype would become the ancestral phenotype when the next mutation is examined
+					}
 				}
 			}
+			pt[[n]][,t]=g2p(gt[[n]][,t],coeff)
 		}
-		pt[[n]][,t]=g2p(gt[[n]][,t],coeff)
 	}
-}
-
-gt_end=matrix(0,nrow=Nrep,ncol=6) # Genotypic values of all lineages at the end of simulation
-pt_end=matrix(0,nrow=Nrep,ncol=6) # Phenotypes of all lineages at the end of simulation
-# Create a new directory to store output files
-dir_new=paste(ncc,"_",paste(width[1],width[2],width[3],sep=""),sep="")
-dir.create(dir_new)
-setwd(paste("./",dir_new,sep="")) # Change working directory to the newly created one
-for(n in 1:Nrep){
-	gt_end[n,]=gt[[n]][,(T+1)] # End-point genotypic valyes of this lineage
-	pt_end[n,]=pt[[n]][,(T+1)] # End-point phenotype of the lineage
-
-	# Create new directory for this lineage
-	dir_new=paste("out_",n,sep="")
-	dir.create(dir_new)
-	fn1=paste(dir_new,"/gt_all.txt",sep="");write.table(gt[[n]],file=fn1,sep="\t") # Genotyic values through time for this lineage
-	fn2=paste(dir_new,"/pt_all.txt",sep="");write.table(pt[[n]],file=fn2,sep="\t") # Phenotypes through time for this lineage
-	#setwd("..") # Back to parental directory before going to the next lineage
-}
-#setwd("..")
-# Output end-point genotypic values and phenotypes
-fn1=paste("gt_",paste(width[1],width[2],width[3],sep=""),".txt",sep="");write.table(gt_end,file=fn1,sep="\t")
-fn2=paste("pt_",paste(width[1],width[2],width[3],sep=""),".txt",sep="");write.table(pt_end,file=fn2,sep="\t")
-# Correlation matrix for all genotypic values and phenotypes
-m.out=cov2cor(cov(data.frame(gt_end,pt_end)))
-write.table(m.out,file="cor.txt",sep="\t")
-
-# Data matrix that contains variances of all traits through time; the last column contains end-point variances that would be used in most analyses
-var.all=matrix(0,nrow=ngene*4,ncol=(T+1))
-for(t in 2:(T+1)){
-	d=matrix(0,nrow=Nrep,ncol=ngene*4)
-	for(n in 1:Nrep){
-		d[n,1:6]=gt[[n]][,t]
-		d[n,7:12]=pt[[n]][,t]
+	# Go through simulated data and extract information of interest
+	for(i in 1:length(T.rec)){
+		t=T.rec[i]
+		out.all[row+i-1,7]=t
+		d=matrix(0,nrow=Nrep,ncol=12)
+		for(n in 1:Nrep){
+			d[n,1:6]=gt[[n]][,(t+1)]
+			d[n,7:12]=pt[[n]][,(t+1)]
+		}
+		# Write variances in genotypic values and phenotypes
+		for(column in 8:19){
+			out.all[row+i-1,column]=var(d[,column-7])
+		}
+		# Write correlations between genotypic values and between phenotypes
+		# Write ratio of variances of mRNA and protein levels
+		# Only end point values for each interaction parameter combination are calculated and written
+		if(t==T){
+			out.all[row+i-1,20]=cor(d[,1],d[,2])
+			out.all[row+i-1,21]=cor(d[,3],d[,4])
+			out.all[row+i-1,22]=cor(d[,5],d[,6])
+			out.all[row+i-1,23]=cor(d[,7],d[,8])
+			out.all[row+i-1,24]=cor(d[,9],d[,10])
+			out.all[row+i-1,25]=cor(d[,11],d[,12])
+			out.all[row+i-1,26]=out.all[row+i-1,15]/out.all[row+i-1,14]
+			out.all[row+i-1,27]=out.all[row+i-1,17]/out.all[row+i-1,16]
+			out.all[row+i-1,28]=out.all[row+i-1,19]/out.all[row+i-1,18]
+		}
 	}
-	for(i in 1:(ngene*4)){
-		var.all[i,t]=var(d[,i])
-	}
-}
-write.table(var.all,file="var_all.txt",sep="\t")
-
+	row=row+T/10
 }
 
+# Generate output file
+write.table(data.frame(out.all),file="out_interaction_3g.txt",sep="\t")
+
+# Generate a concise output file that contains end point values only
+out.end=out.all[which(out.all[,7]==T),]
+out.end=data.frame(out.end[,1:6],out.end[8:28])
+write.table(out.end,file="out_interaction_3g_end.txt",sep="\t")
