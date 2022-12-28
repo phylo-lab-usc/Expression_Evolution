f388877100ad9c1e993ba374d2a461a96f29f3aa
Rex Jiang <61212763+RexJiangEvoBio@users.noreply.github.com>
2022-12-28 14:22:52 -0800

    Add files via upload
    Adding code for simulating three interacting genes that go through multiple pre-specified combinations of interaction parameter values.

---
+++ test/sim_interaction_3g_auto.R
@@ -0,0 +1,192 @@
+# Modeling coevolution between mRNA and protein levels for three interacting genes
+# Both genes are under direct selection
+# Automatically run many combinations of interaction parameters
+
+library(gtools) # The package is used to deal with permutations (combinations)
+
+Ne=1e3 # Effective population size
+dcr=1;dcp=1 # Decay rates of mRNA and protein (assumed to be the same for all genes)
+ngene=3 # Number of genes under consideration
+
+# Interaction parameters
+# All permutations of 0, a positive number and a negative number
+# Numbers represent elements [1,2], [1,3], [2,1], [2,3], [3,1], [3,2], respectively
+# Only combinations of -0.5,0,and 0.5 are considered because there would otherwise be too many combinations
+permut.all=permutations(3,6,c(-0.5,0,0.5),repeats.allowed=TRUE)
+
+# Convert a vector of interaction parameters to the corresponding 3x3 matrix
+v2m <- function(v){
+	m=matrix(0,nrow=3,ncol=3)
+	m[1,2:3]=v[1:2]
+	m[2,1]=v[3]
+	m[2,3]=v[4]
+	m[3,1:2]=v[5:6]
+	return(m)
+}
+
+# Fitness function
+# Calculate fitness given distance to the optimal phenotype and shape of fitness function
+fitness <- function(d,a){
+	w=rep(0,length(d))
+	for(i in 1:length(d)){
+		if(a[i]!=0){
+			w[i]=dnorm(d[i],mean=0,sd=a[i])/dnorm(0,mean=0,sd=a[i]) # Calculate a fitness value for each trait
+		}
+	}
+	w=w[which(a!=0)] # Remove traits that do not affect fitness
+	w=prod(w)^(1/length(w)) # Overall fitness calculated as geometric mean of fitness values calculated from individual traits
+	return(w)
+}
+
+# Fixation probability
+# Calculate fixation probability given ancestral and mutant phenotypes (distances to optimum) and SD of Gaussian fitness function
+fix.prob <- function(x1,x2,a){
+	wa=fitness(x1,a) # Calculate ancestral fitness from ancestral phenotype
+	wm=fitness(x2,a) # Calculate mutant fitness from mutant phenotype
+	if(wa>0){
+		s=(wm/wa)-1 # Coefficient of selection
+		if(s==0){
+			p=1/(2*Ne)
+		}else{
+			p=(1-exp(-2*s))/(1-exp(-4*Ne*s)) # Fixation probability
+		}
+	}else{
+		if(wm==0){ # If mutant fitness is also recognized as 0 by R, the mutation is considered neutral.
+			p=1/(2*Ne)
+		}else{ # If mutant fitness is not recognized as 0 by R, the mutation is considered strongly beneficial and would always fix.
+			p=1
+		}
+	}
+	return(p)
+}
+
+# Genotype-phenotype map
+# Return the phenotype (equilibrium mRNA and protein abundances) given genotypic values and interaction parameters
+# Input and output both in log scale
+g2p <- function(gt,coeff){
+	ngene=length(gt)/2
+	right=rep(0,length(gt))
+	mat=matrix(0,nrow=length(gt),ncol=length(gt))
+	for(i in 1:ngene){
+		right[2*i-1]=log(dcr)-gt[2*i-1] # Element 2*i-1 of the right-hand term, B
+		right[2*i]=log(dcp)-gt[2*i] # Element 2*i of the right-hand term, B
+		for(j in 1:ngene){
+			# If the row number is odd and the column number is even, the entry is an interaction parameter
+			mat[2*i-1,2*j]=coeff[j,i]
+			# If both the row number and the column number are odd, the entry is zero
+		}
+		mat[2*i-1,2*i-1]=-1 # Diagonal elements are all -1
+		mat[2*i,2*i-1]=1 # If the row number is even and the column number is odd, the entry is 1
+		mat[2*i,2*i]=-1 # Diagonal elements are all -1
+	}
+	if(det(mat)!=0){
+		pt=solve(mat,right) # Solve Ax=B equation system
+		return(pt) # Return log scale phenotypes
+	}
+}
+
+# Remove interaction parameter combinations that cannot produce a steady state phenotype
+rm=rep(1,nrow(permut.all))
+for(i in 1:nrow(permut.all)){
+	pt=g2p(rep(1,6),v2m(permut.all[i,]))
+	if(length(pt)==0){
+		rm[i]=0
+	}
+}
+
+# Rows of the output data that correspond to a certain type of phenotype
+# Used to conveniently extract subset of data during simulations and analyses
+row.transcription=c() # Rows of output data that are transcription rates
+row.translation=c() # Rows of output data that are translation rates
+row.mRNA=c() # Rows of output data that are mRNA levels
+row.protein=c() # Rows of output data that are protein levels
+for(i in 1:ngene){
+	row.transcription=c(row.transcription,2*i-1)
+	row.translation=c(row.translation,2*i)
+	row.mRNA=c(row.mRNA,2*i-1)
+	row.protein=c(row.protein,2*i)
+}
+
+lambda.all=rep(1,2*ngene) # Mutation rates for each trait
+sig.all=rep(0.1,2*ngene) # SD of mutation effect size
+
+width=rep(1,ngene) # Width of fitness function for each ngene
+opt=rep(0,ngene)
+
+T=1e4 # Duration of simulation for each lineage
+T.rec=(1:(T/10))*10 # Time points at which results would be written into the output matrix
+Nrep=100 # Number of lineages
+
+for(ncc in 1:length(coeff.all.m)){ # Go through the list of interaction matrices
+
+coeff=coeff.all.m[[ncc]]
+gt=list();pt=list()
+
+for(n in 1:Nrep){
+	gt[[n]]=matrix(0,nrow=2*ngene,ncol=(T+1)) # Mean genotypic values of the n-th lineage through time 
+	pt[[n]]=matrix(0,nrow=2*ngene,ncol=(T+1)) # Mean phenotypes of the n-th lineage through time 
+	pt[[n]][,1]=g2p(gt[[n]][,1],coeff) # Calculate the starting phenotype
+	for(t in 2:(T+1)){
+		gt[[n]][,t]=gt[[n]][,(t-1)]
+		nm=rpois(1,lambda=sum(u)) # Total number of mutations that would occur in this time step
+		if(nm>0){
+			for(i in 1:nm){
+				gt_ances=gt[[n]][,t]
+				pt_ances=g2p(gt_ances,coeff) # Calcuate ancestral phenotype from the ancestral genotype
+				gt_mutant=gt_ances
+				type=sample(1:(2*ngene),1,prob=u/sum(u)) # Decide which trait the mutation affects
+				effect=rnorm(1,mean=0,sd=sigma[type])
+				gt_mutant[type]=gt_mutant[type]+effect # Add the mutation's effect to the affected trait
+				pt_mutant=g2p(gt_mutant,coeff) # Calculate mutant phenotype from the mutant genotype
+				pf=fix.prob(pt_ances[row.protein],pt_mutant[row.protein],width) # Fixation probability of the mutation
+				if.fix=rbinom(n=1,size=1,prob=pf) # Decide if the mutation would fix
+				if(if.fix==1){
+					gt[[n]][,t]=gt_mutant # If the mutation is fixed, the mutant genotype would become the ancestral phenotype when the next mutation is examined
+				}
+			}
+		}
+		pt[[n]][,t]=g2p(gt[[n]][,t],coeff)
+	}
+}
+
+gt_end=matrix(0,nrow=Nrep,ncol=6) # Genotypic values of all lineages at the end of simulation
+pt_end=matrix(0,nrow=Nrep,ncol=6) # Phenotypes of all lineages at the end of simulation
+# Create a new directory to store output files
+dir_new=paste(ncc,"_",paste(width[1],width[2],width[3],sep=""),sep="")
+dir.create(dir_new)
+setwd(paste("./",dir_new,sep="")) # Change working directory to the newly created one
+for(n in 1:Nrep){
+	gt_end[n,]=gt[[n]][,(T+1)] # End-point genotypic valyes of this lineage
+	pt_end[n,]=pt[[n]][,(T+1)] # End-point phenotype of the lineage
+
+	# Create new directory for this lineage
+	dir_new=paste("out_",n,sep="")
+	dir.create(dir_new)
+	fn1=paste(dir_new,"/gt_all.txt",sep="");write.table(gt[[n]],file=fn1,sep="\t") # Genotyic values through time for this lineage
+	fn2=paste(dir_new,"/pt_all.txt",sep="");write.table(pt[[n]],file=fn2,sep="\t") # Phenotypes through time for this lineage
+	#setwd("..") # Back to parental directory before going to the next lineage
+}
+#setwd("..")
+# Output end-point genotypic values and phenotypes
+fn1=paste("gt_",paste(width[1],width[2],width[3],sep=""),".txt",sep="");write.table(gt_end,file=fn1,sep="\t")
+fn2=paste("pt_",paste(width[1],width[2],width[3],sep=""),".txt",sep="");write.table(pt_end,file=fn2,sep="\t")
+# Correlation matrix for all genotypic values and phenotypes
+m.out=cov2cor(cov(data.frame(gt_end,pt_end)))
+write.table(m.out,file="cor.txt",sep="\t")
+
+# Data matrix that contains variances of all traits through time; the last column contains end-point variances that would be used in most analyses
+var.all=matrix(0,nrow=ngene*4,ncol=(T+1))
+for(t in 2:(T+1)){
+	d=matrix(0,nrow=Nrep,ncol=ngene*4)
+	for(n in 1:Nrep){
+		d[n,1:6]=gt[[n]][,t]
+		d[n,7:12]=pt[[n]][,t]
+	}
+	for(i in 1:(ngene*4)){
+		var.all[i,t]=var(d[,i])
+	}
+}
+write.table(var.all,file="var_all.txt",sep="\t")
+
+}
+
